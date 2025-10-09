b8d24996b1fe99c0654a0e69ac32234f234b3502
Eliza Cohn <eliza.cohn1@gmail.com>
2025-10-09 09:10:01 -0400

    convex hull approx 2d complete

--- convexhull-det-sim.jl
+++ convexhull-det-sim.jl
@@ -22,27 +22,31 @@ global g = 9.8           # acceleration due to gravity [m/s^2]
 
 # ----------------- UNIT 1 SITE PARAMETERS (BONNEVILE)  ----------------- #
 
-global min_ut1 = s2hr*cfs_to_m3s(30600)     # min water release rate  [m3/hr] 
-global max_ut1 = s2hr*cfs_to_m3s(134100)    # max water release rate [m3/hr] 
-global min_Vt1 = ft_to_m(70)                # min forebay elevation levels [m]
-global max_Vt1 = ft_to_m(77)                # max forebay elevation levels [m]
-global RR_dn1 = s2hr*cfs_to_m3s(-6300)      # down ramp rate limit [m3/hr] 
-global RR_up1 = s2hr*cfs_to_m3s(6000)       # up ramp rate limit [m3/hr]
-global a1 = 0.0026;                         # hydraulic head parameter 1 
-global b1 = 0.4404;                         # hydraulic head parameter 2 
-global F1 = 1154                        # nameplate capacity [MW]
+a1 = 0.0026;                         # hydraulic head parameter 1 
+b1 = 0.4404;                         # hydraulic head parameter 2 
+min_ut1 = s2hr*cfs_to_m3s(30600)     # min water release rate  [m3/hr] 
+max_ut1 = s2hr*cfs_to_m3s(134100)    # max water release rate [m3/hr] 
+min_h1 = ft_to_m(70)                 # min forebay elevation levels [m]
+max_h1 = ft_to_m(77)                 # max forebay elevation levels [m]
+min_V1 = (min_h1/a1)^(1/b1)          # min volume [m3]
+max_V1 = (max_h1/a1)^(1/b1)          # max volume [m3]
+RR_dn1 = s2hr*cfs_to_m3s(-6300)      # down ramp rate limit [m3/hr] 
+RR_up1 = s2hr*cfs_to_m3s(6000)       # up ramp rate limit [m3/hr]
+F1 = 1154                            # nameplate capacity [MW]
 
 # ----------------- UNIT 2 SITE PARAMETERS (DALLES)  ----------------- #
 
-global min_ut2 = s2hr*cfs_to_m3s(51700)     # min water release rate  [m3/hr] 
-global max_ut2 = s2hr*cfs_to_m3s(161600)    # max water release rate [m3/hr] 
-global min_Vt2 = ft_to_m(155)               # min forebay elevation levels [m]
-global max_Vt2 = ft_to_m(165)               # max forebay elevation levels [m]
-global RR_dn2 = s2hr*cfs_to_m3s(-4900)      # down ramp rate limit [m3/hr] 
-global RR_up2 = s2hr*cfs_to_m3s(4700)       # up ramp rate limit [m3/hr]
-global a2 = 4.1;                         # hydraulic head parameter 1 
-global b2 = 0.134;                       # hydraulic head parameter 2 
-global F2 = 1780                         # nameplate capacity [MW]
+a2 = 4.1;                            # hydraulic head parameter 1 
+b2 = 0.134;                          # hydraulic head parameter 2 
+min_ut2 = s2hr*cfs_to_m3s(51700)     # min water release rate  [m3/hr] 
+max_ut2 = s2hr*cfs_to_m3s(161600)    # max water release rate [m3/hr] 
+min_h2 = ft_to_m(155)                # min forebay elevation levels [m]
+max_h2 = ft_to_m(165)                # max forebay elevation levels [m]
+min_V2 = (min_h2/a2)^(1/b2)          # min volume [m3]
+max_V2 = (max_h2/a2)^(1/b2)          # max volume [m3]
+RR_dn2 = s2hr*cfs_to_m3s(-4900)      # down ramp rate limit [m3/hr] 
+RR_up2 = s2hr*cfs_to_m3s(4700)       # up ramp rate limit [m3/hr]
+F2 = 1780                            # nameplate capacity [MW]
 
 # -----------------  DATA LOAD  ----------------- #
 println("--- DATA LOAD BEGIN ---")
@@ -51,19 +55,34 @@ gage, inflow, storage = fullsim_dataload();
 
 # Filter Dataset
 start_date = DateTime("2023-06-01T00:00:00")
-end_date   = DateTime("2023-06-01T06:00:00") 
+end_date   = DateTime("2023-06-01T06:00:00") # Run for T = 6 hrs
 inflow_s = inflow[(inflow.datetime .>= start_date) .&& (inflow.datetime .<= end_date), :]
-N = nrow(inflow_s);
+T = nrow(inflow_s);
 
 # Storage Levels [m3]
 V0_01 = af_to_m3(697000) #storage.bon_S_m3[end]
 V0_02 = storage.tda_S_m3[end]
 
 # Inflow [m3/hr] 
-q1 = s2hr*inflow_s.bon_inflow_m3s         # historic downstream inflow to 01 Bonneville
+q1 = s2hr*inflow_s.bon_inflow_m3s         # historic downstream inflow to 01 Bonneville [m3/s] --> [m3/hr]
 q2 = s2hr*inflow_s.tda_inflow_m3s         # historic upstream inflow to 02 Dalles Dam [m3/s] --> [m3/hr]
 
 println("--- DATA LOAD COMPLETE ---")
+
+## ----------- DATA SAMPLING ----------- ##
+
+M = 100 
+N = 15
+
+# Independent Sample Vectors
+V_sample = range(min_V1, max_V1, length = M)
+u_sample = range(min_ut1, max_ut1, length = N)
+
+# Define Hydraulic Head Function
+f1(V) = a1 * (V^b1)
+
+# Precomputed Matrix of Power Outputs
+P = [eta * g * rho_w * f1(V) * u for u in u_sample, V in V_sample] / (3.6e9)
 
 ## ----------- RUN BASELINE ----------- ##
 
@@ -71,66 +90,34 @@ println("--- SIMULATION BEGIN ---")
 
 # Create the optimization model
 model = Model(Gurobi.Optimizer)
-# model = Model(Ipopt.Optimizer)
-# set_silent(model) 
 
+### Unit 01: Bonneville Dam (Downstream)
 ## Define variables
-# Unit 01: Bonneville Dam (Downstream)
-@variable(model, V1[1:N] >= 0) # Reservoir Volume [m3]
-@variable(model, p1[1:N] >= 0) # Generation [MWh]
-@variable(model, u1[1:N])      # Generation Outflow [m3/hr]
-@variable(model, s1[1:N] >= 0) # Spill Outflow [m3/hr]
-set_upper_bound.(u1, max_ut1)
-set_lower_bound.(u1, min_ut1)
-
-#=
-# Unit 02: The Dalles Dam (Upstream)
-@variable(model, V2[1:N] >= 0) # Reservoir Volume [m3]
-@variable(model, p2[1:N] >= 0) # Generation [MWh]
-@variable(model, u2[1:N])      # Generation Outflow [m3/hr]
-@variable(model, s2[1:N] >= 0) # Spill Outflow [m3/hr]
-set_upper_bound.(u2, max_ut2)
-set_lower_bound.(u2, min_ut2)
-=#
+@variable(model, s1[1:T] >= 0) # Spill Outflow [m3/hr]
+@variable(model, lam1[1:N, 1:M, 1:T] >= 0)  # lambda matrix
 
 ## Initial conditions
-# Unit 1
-@constraint(model, MassBalInit1, V1[1] == V0_01)
-@constraint(model, RampRateInit1, u1[1] == min_ut1)
+@constraint(model, MassBalInit1, sum(lam1[i,j,1] * V_sample[j] for i=1:N, j=1:M) == V0_01)
+@constraint(model, RampRateInit1, sum(lam1[i,j,1] * u_sample[i] for i=1:N, j=1:M) == min_ut1)
 
-#=
-# Unit 2
-@constraint(model, MassBalInit2, V2[1] == V0_02)
-@constraint(model, RampRateInit2, u2[1] == min_ut2)
-=#
+## Expressions
+@expression(model, V1[t=1:T], sum(lam1[i,j,t] * V_sample[j] for i=1:N, j=1:M))
+@expression(model, u1[t=1:T], sum(lam1[i,j,t] * u_sample[i] for i=1:N, j=1:M))
+@expression(model, p1[t=1:T], sum(lam1[i,j,t] * P[i,j]      for i=1:N, j=1:M))
+
+## Constraints
+@constraint(model, MassBal1[t in 2:T], V1[t] == V1[t-1] + q1[t] - u1[t] - s1[t])
+@constraint(model, Release1[t in 2:T], min_ut1 <= u1[t] <= max_ut1)
+@constraint(model, RampRate1[t in 2:T], RR_dn1 <= u1[t] - u1[t-1] <= RR_up1)
+@constraint(model, FeederCap1[t in 1:T], 0 <= p1[t] <= F1)
+@constraint(model, Volume1[t in 1:T], min_V1 <= V1[t] <= max_V1)
+@constraint(model, lambda1[t in 1:T], sum(lam1[:,:,t]) == 1)
+
+### Unit 02: Dalles Dam (Downstream)
+# Left out for now until approximation logic is finalized
 
 # Objective function
 @objective(model, Max, sum(p1 )) ####+ p2)) 
-
-## Constraints
-# Unit 1
-@constraint(model, MassBal1[t in 2:N], V1[t] == V1[t-1] + q1[t] - u1[t] - s1[t])
-@constraint(model, ReleaseEnergy1[t in 1:N], p1[t] <= (eta * g * rho_w * u1[t] * a1 * (V1[t]^b1))/(3.6e9))
-@constraint(model, Release1[t in 2:N], min_ut1 <= u1[t] <= max_ut1)
-@constraint(model, RampRate1[t in 2:N], RR_dn1 <= u1[t] - u1[t-1] <= RR_up1)
-@constraint(model, FeederCap1[t in 1:N], 0 <= p1[t] <= F1)
-@constraint(model, Volume1[t in 1:N], min_Vt1 <= a1 * (V1[t]^b1) <= max_Vt1)
-
-#=
-# Unit 2
-@constraint(model, MassBal2[t in 2:N], V2[t] == V2[t-1] + q2[t] - u2[t] - s2[t])
-@constraint(model, ReleaseEnergy2[t in 1:N], p2[t] <= (eta * g * rho_w * u2[t] * a2 * (V2[t]^b2))/(3.6e9))
-@constraint(model, Release2[t in 2:N], min_ut2 <= u2[t] <= max_ut2)
-@constraint(model, RampRate2[t in 2:N], RR_dn2 <= u2[t] - u2[t-1] <= RR_up2)
-@constraint(model, FeederCap2[t in 1:N], 0 <= p2[t] <= F2)
-@constraint(model, Volume2[t in 1:N], min_Vt2 <=  a2 * (V2[t]^b2) <= max_Vt2)
-=#
-
-# Set parameters for piecewise-linear approximation (FASTER)
-set_attribute(model, "FuncNonlinear", 0)    # Use piecewise-linear (default)
-set_attribute(model, "FuncPieces", 4)       
-set_attribute(model, "FuncPieceError", 5e-2)  # Allow larger errors
-    
 
 # Solve the optimization problem
 optimize!(model)
@@ -145,17 +132,10 @@ println(obj)
 stamp = Dates.format(now(), "mm-dd-yyyy HH.MM.SS") * " CONVEX HULL";
 dir = "./plots/" ;
 path = dir * stamp;
-# path = "./plots/06-23-2025 14.25.56"
 mkdir(path)
 
 hh1 =  (eta * g * rho_w * value.(u1) * a1 .* (value.(V1).^b1))/(3.6e9)
 h1 = a1 .* (value.(V1).^b1)
-sim_plots(path, "Unit1", N, value.(u1), value.(s1), value.(p1), value.(V1), q1, h1, F1, hh1, min_ut1, max_ut1, min_Vt1, max_Vt1)
-
-#=
-hh2 =  (eta * g * rho_w * value.(u2) * a2 .* (value.(V2).^b2))/(3.6e9)
-h2 = a2 .* (value.(V2).^b2)
-sim_plots(path, "Unit2", N, value.(u2), value.(s2), value.(p2), value.(V2), q2, h2, F2, hh2, min_ut2, max_ut2, min_Vt2, max_Vt2)
-=#
+sim_plots(path, "Unit1", T, value.(u1), value.(s1), value.(p1), value.(V1), q1, h1, F1, hh1, min_ut1, max_ut1, min_h1, max_h1)
 
 println("--- SIMULATION COMPLETE ---")
