e74d9423b1b9f3d16c8c30ee9b77dc85eca07ef3
Eliza Cohn <eliza.cohn1@gmail.com>
2026-04-15 10:12:46 -0400

    plotting

--- simulations/SOC-sims/plotRiskSpread.m
+++ simulations/SOC-sims/plotRiskSpread.m
@@ -4,45 +4,60 @@ function plotRiskSpread(alphas_MC)
     tt = 1:T;
 
     % Mean colors
-    cMean{1} = [0.0000, 0.4470, 0.7410];   % dark blue
-    cMean{2} = [0.8500, 0.3250, 0.0980];   % orange/red
+    cMean{1} = [0.0000, 0.4470, 0.7410];   % blue
+    cMean{2} = [0.8500, 0.3250, 0.0980];   % orange
     cMean{3} = [0.9290, 0.6940, 0.1250];   % yellow
 
-    % Light trajectory colors
-    cLite{1} = [0.65, 0.82, 0.97];         % light blue
-    cLite{2} = [0.98, 0.75, 0.60];         % light orange
-    cLite{3} = [0.98, 0.90, 0.60];         % light yellow
+    % Lighter fill colors for shadow bands
+    cFill{1} = [0.65, 0.82, 0.97];
+    cFill{2} = [0.98, 0.75, 0.60];
+    cFill{3} = [0.98, 0.90, 0.60];
 
-    figure('Color','w'); 
+    figure('Color','w');
     hold on; box on; grid on;
 
-    % --- plot MC trajectories first ---
-    for i = 1:n
-        for k = 1:K
-            plot(tt, alphas_MC(:,i,k), ...
-                'Color', cLite{i}, ...
-                'LineWidth', 0.75, ...
-                'HandleVisibility','off');
-        end
-    end
+    h = gobjects(n+1,1);
 
-    % --- plot mean trajectories on top ---
-    h = gobjects(n,1);
+    % --- shaded IQR band + mean line for each unit ---
     for i = 1:n
-        alpha_mean = mean(alphas_MC(:,i,:), 3, 'omitnan');
+        A = squeeze(alphas_MC(:,i,:));   % T x K
+
+        alpha_mean = median(A, 2, 'omitnan');
+        alpha_q25  = prctile(A', 25)';   % T x 1
+        alpha_q75  = prctile(A', 75)';
+
+        % Shadow band
+        fill([tt, fliplr(tt)], ...
+             [alpha_q25', fliplr(alpha_q75')], ...
+             cFill{i}, ...
+             'FaceAlpha', 0.35, ...
+             'EdgeColor', 'none', ...
+             'HandleVisibility', 'off');
+
+        % Mean line
         h(i) = plot(tt, alpha_mean, ...
             'Color', cMean{i}, ...
-            'LineWidth', 2.2);
+            'LineWidth', 2.8);
     end
 
-    xlabel('Time Step');
-    ylabel('\alpha_i');
-    title('Monte Carlo Risk Allocation Trajectories');
-    legend(h, {'Unit 1 Mean','Unit 2 Mean','Unit 3 Mean'}, ...
-        'Location','best');
+    % --- Bonferroni reference line ---
+    h(n+1) = yline(1/n, '--', ...
+        'Color', [0.3 0.3 0.3], ...
+        'LineWidth', 2.0);
+
+    xlabel('Time Step', 'FontSize', 16);
+    ylabel('\alpha_i', 'FontSize', 16);
+
+    legend(h, {'Unit 1 Mean', 'Unit 2 Mean', 'Unit 3 Mean', 'Bonferroni: 1/n'}, ...
+        'Location', 'best', ...
+        'FontSize', 13, ...
+        'Box', 'on');
 
     xlim([1 T]);
-    ylim([0 1]);   % remove if needed
-    set(gca,'FontSize',14);
+    ylim([0 0.5]);   % tighter axis to emphasize convergence
+    set(gca, ...
+        'FontSize', 15, ...
+        'LineWidth', 1.2, ...
+        'Box', 'on');
 
 end
--- simulations/SOC-sims/simulation.m
+++ simulations/SOC-sims/simulation.m
@@ -40,7 +40,7 @@ eps = 0.05;         % risk tolerance
 % ========================================================================
 
 % Initialize settings (season, drought type, lin a, pprox, uncertainty, sln alg, volume price)
-simSettings = initSimSettings("dry", "extended", "pwl", "ddu", "jcc-ssh", "none");
+simSettings = initSimSettings("dry", "extended", "pwl", "diu", "jcc-ssh", "none");
 
 % Extract forecasting coefficients 
 modelparams = modelparams(strcmp({modelparams.season}, simSettings.season));
